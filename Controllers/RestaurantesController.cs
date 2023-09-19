4e14bb20b7feacc7fa8c76abd68c17ef20ad254e
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-09-19 19:57:49 -0300

    Criada rota HttpGet

---
+++ Controllers/RestaurantesController.cs
@@ -0,0 +1,26 @@
+﻿using DriveXpress.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriveXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RestaurantesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Restaurantes.ToListAsync();
+            return Ok(model);
+        }
+    }
+}
