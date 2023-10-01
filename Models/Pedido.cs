c2bf4944a9a567ce97e9839040a9dea88637363b
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-10-01 17:20:27 -0300

    Inserida classe Pedido

---
+++ Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveXpress.Models
+{
+    [Table("Pedido")]
+    public class Pedido : LinksHATEOS
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NomeUsuario { get; set; }        
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public int Quantidade { get; set; }        
+        [Required]
+        public double Valor { get; set; }
+        
+        [Required]
+        public int RestauranteId { get; set; } //pedido pertence a um unico restaurante (pedido associado a Key de restaurante)
+
+        public Produto Produto { get; set; } //retorna produtos cadastrados
+    }
+}
